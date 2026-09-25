57a7b4231db6a893e76e8fe57c94c8a72da4aac3
voxbox board action <actions@users.noreply.github.com>
2026-09-25 07:05:39 +0000

    Board from voxbox-ue 38d7a9a

--- jobs.js
+++ jobs.js
@@ -1,5 +1,5 @@
 window.VOX_BOARD = {
-"updated": "2026-09-25 06:47 UTC",
+"updated": "2026-09-25 07:05 UTC",
 "machines": {
   "ben-3070": "Ben's Win (RTX 3070 8GB)",
   "james-3080": "James Win (RTX 3080 10GB)",
